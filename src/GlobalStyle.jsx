3dbaf0c08871ef2ed1f45df5913726b9496988ec
Kong-E <kng001016@dgu.ac.kr>
2023-07-14 05:29:07 +0900

    feat: 디렉토리 구조 설정 및 Header 컴포넌트 구성

---
+++ src/GlobalStyle.jsx
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: "Inter", sans-serif;
+    font-size: 16px;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export default GlobalStyle;
