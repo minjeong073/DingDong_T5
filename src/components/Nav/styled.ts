d90a9816357e89db9effff2469a9ddbc3a7d60a6
g2Min <jimin9320@gmail.com>
2023-07-16 00:37:57 +0900

    style: "nav바 UI변경"

--- src/components/Nav/styled.ts
+++ src/components/Nav/styled.ts
@@ -11,11 +11,15 @@ export const NavItem = styled.div`
   display: flex;
   justify-content: flex-end;
   align-items: center;
+  font-size: 1.5rem;
+  font-family: 'Roboto Slab', serif;
+  font-weight: bold;
   width: 100%;
   height: 47px;
   border-bottom: 1px solid #e2e8f0;
   padding-right: 19px;
   &:hover {
     cursor: pointer;
+    color: gray;
   }
 `;
