0af4bf73225aa23a2232d73a61cd94508a17166f
g2Min <jimin9320@gmail.com>
2023-07-16 17:17:52 +0900

    feat: "table 셀사이의 공백제거, 셀높이 고정"

--- src/components/ArticlesTable.tsx
+++ src/components/ArticlesTable.tsx
@@ -3,22 +3,30 @@ import React from 'react';
 import dummy from "../db/articles.json";
 import { isTemplateSpan } from "typescript";
 
+const itemsPerPage = 5;
+
 export const ArticlesTable = () => {
 
   return(
     <Table>
       {dummy.articles.map((item) => (
-        <tr key={item.id}>                 
-          <Info>
-            <Box> <Div>{item.votes}</Div> <Span>투표수</Span></Box>
-            <Box><Div>{item.answers}</Div> <Span>답변수</Span></Box>
-            <Box><Div>{item.views}</Div> <Span>조회수</Span></Box>
-          </Info>          
-          <Context>
-            <Title>{item.title}</Title>
-          </Context>
-                   
-        </tr>
+        <TableRow key={item.id}>
+          <TableCell>                
+            <Info>
+              <Box> <Div>{item.votes}</Div> <Span>투표수</Span></Box>
+              <Box><Div>{item.answers}</Div> <Span>답변수</Span></Box>
+              <Box><Div>{item.views}</Div> <Span>조회수</Span></Box>
+            </Info>          
+            <Context>
+              <Title>{item.title}</Title>
+              <Addition>
+                <HashTag>{item.hashtag}</HashTag>
+                <Author>{item.author}</Author>
+                <Date>{item.date}</Date>
+              </Addition>
+            </Context>
+          </TableCell>          
+        </TableRow>
       ))}
 
     </Table>
@@ -38,11 +46,25 @@ const Table = styled.table`
   
 `;
 
-const Info = styled.td`
+const TableRow = styled.tr`
+  display:flex;
+`;
+
+
+const TableCell = styled.td`
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  height: 180px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+const Info = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  border: 1px solid black;
 `;
 
 const Box = styled.div`
@@ -51,7 +73,8 @@ const Box = styled.div`
   margin: 12px;
   width: 100px;
   height: 32px;
-  border: 1px solid black;
+  border-radius: 5px;
+
 `;
 
 const Div = styled. div`
@@ -63,13 +86,28 @@ const Span= styled.div`
   font-size: 20px;
 `;
 
-const Context = styled.td`
+const Context = styled.div`
 
 
 `;
 
 const Title = styled.div`
+  margin: 0px 30px 0px 30px;
   border: 1px solid black;
-  font-size: 20px;
-  
+  font-size: 23px;
+  font-weight: medium;
 `;
+
+const Addition = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const HashTag = styled.div`
+  margin: 20px;
+
+`;
+
+const Author = styled.div``;
+
+const Date = styled.div``;
