192b82b2a2422e730497dfe32fe739545d838916
Kong-E <kng001016@dgu.ac.kr>
2023-07-18 00:23:26 +0900

    feat: nav 라우팅 경로 수정

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,16 +2,27 @@ import { useMemo } from "react";
 import { Container, NavItem } from "./styled";
 import { Link } from "react-router-dom";
 
+type navItem = {
+  name: string;
+  src: string;
+};
+
+type navItems = navItem[];
+
 export const Nav = () => {
-  const navItems = useMemo<string[]>(
-    () => ["Home", "Questions", "Replies"],
+  const navItems = useMemo<navItems>(
+    () => [
+      { name: "Home", src: "/" },
+      { name: "Questions", src: "/articles" },
+      { name: "Replies", src: "/replies" },
+    ],
     []
   );
   return (
     <Container>
-      {navItems.map((item) => (
-        <Link to={`/${item.toLowerCase()}`} key={item}>
-          <NavItem>{item}</NavItem>
+      {navItems.map((item, idx) => (
+        <Link to={item.src} key={`${item.name}_${idx}`}>
+          <NavItem>{item.name}</NavItem>
         </Link>
       ))}
     </Container>
