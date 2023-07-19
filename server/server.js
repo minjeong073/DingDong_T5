f26dae59f63516a7c1b0f748e611cbf7860c0db2
KIM MINJEONG <91234288+minjeong073@users.noreply.github.com>
2023-07-19 13:42:48 +0900

    feat: model, route setting

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const port = process.env.PORT || 5001;
+const articleRoute = require('./routes/article');
 
 dotenv.config();
 app.use(express.json());
@@ -14,5 +15,7 @@ mongoose
   })
   .then(() => console.log('MongoDB Connected...'))
   .catch((err) => console.log(err));
+
+app.use('/api/articles', articleRoute);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
