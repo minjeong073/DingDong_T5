3956e51d6163fa0cf52dcdc161da97db17ccdcc6
g2Min <jimin9320@gmail.com>
2023-07-24 03:39:45 +0900

    feat: article api 에 맞게 함수 정의

--- src/api/articlesAPI.js
+++ src/api/articlesAPI.js
@@ -1,11 +1,35 @@
-import api from "./axios";
+import axios from "axios";
+import { allArticles, singleArticleData } from "./url";
 
-export const articlesAPI = async(data) => {
+import {
+  allArticles as _allArticles,
+  singleArticleData as _singleArticleData,
+  hashtagArticle as _hashtagArticle,
+  createSingleArticle as _createSingleArticle,
+  deleteSingleArticle as _deleteSingleArticle
+} from "./url";
 
-  const response = await api.post(
-    `${process.env.REACT_APP_SERVER_URL}/posts/1`,
-    data
-  );
+export const allArticles = () => {
+  return axios.get(_allArticles);
+}
 
-  return response.data;
+export const singleArticleData = (id) => {
+  return axios.get(_singleArticleData.replace('{id}', id));
+}
+
+export const hashtagArticle = (tagName) => {
+  return axios.get(_hashtagArticle.replace('{tagName}', tagName));
+}
+
+export const createSingleArticle = (formData) => {
+  const config_headers = {
+    headers: {
+      "Content-Type" : "application/json",
+    },
+  };
+  return axios.post(_createSingleArticle, formData, config_headers);
+}
+
+export const deleteSingleArticle = (id) => {
+  return axios.delete(_deleteSingleArticle.replace('{id}',id));
 }
