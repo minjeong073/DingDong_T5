81e9554048769884b38bced1e1a2c9ae31713ce5
g2Min <jimin9320@gmail.com>
2023-07-30 03:10:20 +0900

    feat: 해시태그 nav 에 collapsible 속성 부여

--- src/components/HashtagNav/HashtagNav.tsx
+++ src/components/HashtagNav/HashtagNav.tsx
@@ -2,15 +2,23 @@ import dummy from "../../db/articles.json";
 import {
   NavBar,
   Table,
+  Tbody,
   Tr,
-  HashTag
+  Td,
+  HashTag,
+  Button,
+  Img
 } from "./styled";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { QuestionData, QuestionListState } from "../../stores/page-store";
 import type { QuestionDataType } from "../../stores/page-store";
+import React , { useState } from "react";
+import unfold from "../../assets/icon/unfold.svg";
+import fold from "../../assets/icon/fold.svg";
+
 
 export const HashTagNav = () => {
-
+  const [ expanded, setExpanded ] = useState(false);
   const [QuestionData, setQuestionData] =
   useRecoilState<QuestionDataType[]>(QuestionListState);
 
@@ -18,28 +26,36 @@ export const HashTagNav = () => {
   // const Values = useRecoilValue<QuestionDataType[]>(QuestionData).hashtags;
   // console.log(QuestionData[10].hashtags);
 
-
-
   const HashtagArr = dummy.articles.map((item) => item.hashtag);
   const oneHashtag = HashtagArr.flat();
   const onlyHashtag = Array.from(new Set(oneHashtag));
 
+  const onClickExpanded = () =>{
+    setExpanded( !expanded );
+  }
+
+  const foldImage = expanded ? {unfold} : {fold};
+
   return(
     <NavBar>
-      <Table>
-        <tbody>
+      <Table expanded={expanded}>
+        <Tbody >
             {onlyHashtag.map((item, index) => (
               index % 2 === 0 ? (
                 <Tr key={index}>
-                  <td>
+                  <Td>
                   <HashTag key={item}>{item}</HashTag>
                   {index + 1 < onlyHashtag.length ? <HashTag>{onlyHashtag[index + 1]}</HashTag> : null}
-                  </td>
+                  </Td>
                 </Tr>
               ) : null
             ))}
-      </tbody>
+      </Tbody>
     </Table>
+    <Button onClick={onClickExpanded}>
+        {expanded ? "접기"  : "펼치기" }
+        <Img />
+    </Button>
   </NavBar>  
   )
 }
--- src/routes/Layout/Layout.tsx
+++ src/routes/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet} from "react-router-dom";
 import { HashTagNav } from "../../components/HashtagNav";
 import { Header, Nav, RightWrapper } from "../../components";
 import { Container, Root, LeftWrapper } from "./styled";
