450cdb2c4b4d35709e1fba6dec0b5188b0ecfc83
g2Min <jimin9320@gmail.com>
2023-07-30 14:17:34 +0900

    feat: Replies 페이지 기본 컴포넌트 경로 구성

--- src/components/Replies/Comment.tsx
+++ src/components/Replies/Comment.tsx
@@ -1,8 +1,6 @@
-
-
 export const Comment = () => {
   return(
     <>
     </>
-  )
+  );
 };
---
+++ src/components/Replies/CommentList.tsx
@@ -0,0 +1,15 @@
+import { Holder, Text } from "../List/ArticleList/styled";
+
+
+export const CommentList = () =>{
+  return(
+    <>
+      <Holder>
+        <Text>
+          Best Replies
+        </Text>
+      </Holder>
+
+    </>
+  );
+};
