7caea33c4cae78fbb37354306a173e2bfb6ced07
Kong-E <kng001016@dgu.ac.kr>
2023-08-11 03:42:46 +0900

    chore: 푸터에 경로설정

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import {
   CategoryContainer,
   CategoryList,
@@ -19,6 +20,7 @@ import {
 } from './styled';
 
 export const Footer = () => {
+  const navigate = useNavigate();
   return (
     <InformationRoot>
       <UpperContainer>
@@ -31,33 +33,35 @@ export const Footer = () => {
           <Typo>kng001016@dgu.ac.kr</Typo>
         </GiveRunContainer>
         <CategoryContainer>
-          <Title pointer="true">질문하러 가기</Title>
-          <Typo top="25px" bottom="12px">
+          <Title pointer="true" onClick={() => navigate('/articles/write')}>
+            질문하러 가기
+          </Title>
+          <Typo top="25px" bottom="12px" weight={500}>
             All Category
           </Typo>
           <CategoryList>
-            <Typo pointer="true" bottom="10px" weight={500}>
+            <Typo pointer="true" bottom="10px" weight={500} onClick={() => navigate('/')}>
               Home
             </Typo>
-            <Typo pointer="true" bottom="10px" weight={500}>
+            <Typo pointer="true" bottom="10px" weight={500} onClick={() => navigate('/articles')}>
               Questions
             </Typo>
-            <Typo pointer="true" bottom="10px" weight={500}>
+            <Typo pointer="true" bottom="10px" weight={500} onClick={() => navigate('/mypage')}>
               MyPage
             </Typo>
-            <Typo pointer="true" bottom="10px" weight={500}>
+            <Typo pointer="true" bottom="10px" weight={500} onClick={() => navigate('/replies')}>
               Replies
             </Typo>
           </CategoryList>
         </CategoryContainer>
         <DingDongContainer>
-          <Title bottom="15px">DINGDONG</Title>
+          <Title bottom="21px">DINGDONG</Title>
           <Typo>동국대학교 산학연계프로젝트 딩동팀은 탈중앙성을 지향하는</Typo>
           <Typo>여행 커뮤니티를 제작하고 있습니다.</Typo>
         </DingDongContainer>
       </UpperContainer>
       <LowerContainer>
-        <Typo>© 2023 DingDong</Typo>
+        <Typo>© 2023 DINGDONG</Typo>
         <Typo pointer="true">공지사항</Typo>
         <Typo pointer="true">이용약관</Typo>
         <Typo pointer="true">개인정보처리방침</Typo>
