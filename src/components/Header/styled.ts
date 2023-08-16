9ba0c23d989a50e65427b01ea890fb4f044ad429
g2Min <jimin9320@gmail.com>
2023-08-17 04:04:11 +0900

    style: 검색바 스타일 수정

--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -163,7 +163,7 @@ export const SearchInput = styled.input`
 `;
 
 export const DataResult = styled.div`
-  width: 661px;
+  width: 100%;
   height: 200px;
   background-color: white;
   border-radius: 0 0 20px 20px;
