69d229529ec0d5cb77d0aa09a7b908ca4ee548c3
Kong-E <kng001016@dgu.ac.kr>
2023-08-17 04:51:51 +0900

    feat: Layout HashTagNav 보임조건 해제

--- src/routes/Layout/Layout.tsx
+++ src/routes/Layout/Layout.tsx
@@ -8,17 +8,18 @@ export const Layout = () => {
   // /articles나 /mypage를 포함할 때만 푸터가 나타남
   const showFooter = location.pathname.includes('/articles') || location.pathname.includes('/mypage');
 
-  // 해시태그 네비게이션 바가 나타날 URL 조건 설정
+  /*   // 해시태그 네비게이션 바가 나타날 URL 조건 설정
   // /articles나 /replies 일 때만 해시태그 네비게이션 바가 나타남
   const showHashTagNav = location.pathname === '/articles' || location.pathname === '/replies';
-
+ */
   return (
     <Root>
       <Header />
       <Container>
         <LeftWrapper>
           <Nav />
-          {showHashTagNav && <HashTagNav />}
+          {/* {showHashTagNav && <HashTagNav />} */}
+          <HashTagNav />
         </LeftWrapper>
         <Main>
           <Outlet />
