11e1ab1a657218514e721a60c7bb1b6891b02b3e
g2Min <jimin9320@gmail.com>
2023-08-17 04:17:37 +0900

    feat: 태그, 검색 클릭이벤트시 페이지이동

---
+++ src/pages/List/SearchTagPage.tsx
@@ -0,0 +1,10 @@
+import { Articles } from '../../components/List/ArticleList/Articles';
+import { ListContainer } from './styled';
+
+export const SearchTagPage = () => {
+  return (
+    // <ListContainer>
+    <Articles />
+    // </ListContainer>
+  );
+};
