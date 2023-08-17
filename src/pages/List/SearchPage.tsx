871af53e1fb75cde313ca8a3c48a2c415195ed97
g2Min <jimin9320@gmail.com>
2023-08-18 04:24:56 +0900

    feat: 키워드검색 연동

--- src/pages/List/SearchPage.tsx
+++ src/pages/List/SearchPage.tsx
@@ -1,12 +1,31 @@
 import { Articles } from '../../components/List/ArticleList/Articles';
 import { useLocation } from 'react-router-dom';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
+import type { QuestionDataType } from 'stores/page-store';
 
 export const SearchPage = () => {
+  const [SearchData, setSearchData] = useState<QuestionDataType[]>([]);
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-
   const keyword = queryParams.get('keyword');
-  console.log(keyword);
+  // console.log(keyword);
+  // 쿼리부분 변수
+
+  const fetchSearchData = async (keyword:string) => {
+    try{
+      const response = await axios.get(`/api/search?keyword=${encodeURIComponent(keyword)}`);
+      setSearchData(response.data);
+    }catch(error){
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    if(keyword){
+      fetchSearchData(keyword);
+    }; 
+  }, [setSearchData]);
 
   return (
     // <ListContainer>
