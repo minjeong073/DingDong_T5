cb5b260add5b0223e1a0ae45b6da9b21b3052271
g2Min <jimin9320@gmail.com>
2023-08-19 17:22:05 +0900

    feat: 홈화면 query 페이지이동

--- src/pages/List/SearchPage.tsx
+++ src/pages/List/SearchPage.tsx
@@ -1,35 +1,47 @@
-import { Articles } from '../../components/List/ArticleList/Articles';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 import React, {useEffect, useState} from 'react';
 import type { QuestionDataType } from 'stores/page-store';
+import { QuestionRow } from "../../components/QuestionRow";
+import { Table, Tbody } from "../../components/List/ArticlesTable/styled";
 
 export const SearchPage = () => {
   const [SearchData, setSearchData] = useState<QuestionDataType[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const keyword = queryParams.get('keyword');
-  // console.log(keyword);
-  // 쿼리부분 변수
 
-  const fetchSearchData = async (keyword:string) => {
-    try{
-      const response = await axios.get(`/api/search?keyword=${encodeURIComponent(keyword)}`);
-      setSearchData(response.data);
-    }catch(error){
-      console.error(error);
-    }
-  }
 
   useEffect(() => {
-    if(keyword){
+    const fetchSearchData = async(keyword:string) => {
+      try{
+        setIsLoading(true);
+        const response = await axios.get(`/api/search?keyword=${encodeURIComponent(keyword)}`);
+        setSearchData(response.data);
+      }catch(error){
+        console.error('Error fetching data: ', error);
+      }finally{
+        setIsLoading(false);
+      }
+    };
+    if(keyword)
       fetchSearchData(keyword);
-    }; 
-  }, [setSearchData]);
+  }, []);
 
   return (
-    // <ListContainer>
-    <Articles />
-    // </ListContainer>
+    <>
+      {isLoading ? (
+        <div> Loading ... </div>
+          ) : (
+            <Table>
+              <Tbody>
+                {SearchData?.map((item, idx) => (
+                <QuestionRow key={idx} item={item} />
+                ))}
+              </Tbody>
+            </Table>
+      )}          
+    </>
   );
 };
