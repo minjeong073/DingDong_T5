a1ed891b45b4db6e53f2e6598039adf73fc6fe25
g2Min <jimin9320@gmail.com>
2023-08-19 17:27:16 +0900

    chore: 불필요한 주석 삭제

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -1,10 +1,7 @@
 import { Articles } from '../../components/List/ArticleList/Articles';
-import { ListContainer } from './styled';
 
 export const List = () => {
   return (
-    // <ListContainer>
     <Articles />
-    // </ListContainer>
   );
 };
