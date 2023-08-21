560d4b24bfedd977c0668a6757749cae40611c59
g2Min <jimin9320@gmail.com>
2023-08-22 03:48:26 +0900

    chore: 토큰 만료 확인코드 제거

--- src/components/LoginLogoutButton/LoginLogoutButton.tsx
+++ src/components/LoginLogoutButton/LoginLogoutButton.tsx
@@ -55,8 +55,8 @@ export const LoginLogoutButton = () => {
         setLoginState(false);
       }
     }
-    console.log(localStorage.getItem('token'));
-    console.log(loginState);
+    // console.log(localStorage.getItem('token'));
+    // console.log(loginState);
   }, [token]);
 
   return (
