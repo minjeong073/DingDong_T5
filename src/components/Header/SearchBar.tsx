185ee2a8cebcaada8d2953350cb079b841aea803
Kong-E <kng001016@dgu.ac.kr>
2023-08-21 19:14:11 +0900

    fix: 검색어 예외처리 수정

--- src/components/Header/SearchBar.tsx
+++ src/components/Header/SearchBar.tsx
@@ -65,9 +65,11 @@ export const SearchBar: React.FC<SearchProps> = ({ data, placeholder }): JSX.Ele
   };
 
   const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (wordEntered.trim() === '') {
-      alert('검색어가 입력되지 않았습니다.');
-    } else if (e.key === 'Enter') {
+    if (e.key === 'Enter') {
+      if (wordEntered.trim() === '') {
+        alert('검색어가 입력되지 않았습니다.');
+        return;
+      }
       navigateSearchPage();
     }
   };
